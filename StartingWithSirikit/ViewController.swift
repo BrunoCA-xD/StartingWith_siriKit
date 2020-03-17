296e96c66a46c35feb932e1ec8dbd9c38aec491b
Bruno C Ambrosio <bruno.car.ambrosio@gmail.com>
2020-03-16 22:17:28 -0300

    Initial Commit

---
+++ StartingWithSirikit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartingWithSirikit
+//
+//  Created by Bruno Cardoso Ambrosio on 16/03/20.
+//  Copyright © 2020 Bruno Cardoso Ambrosio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
